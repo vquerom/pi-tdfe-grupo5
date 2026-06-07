f1d04ea8233b4f0ee69fc2097d171d309ce364ea
Juan Pablo Leoni <jleoni@udesa.edu.ar>
2026-06-07 11:13:39 -0300

    agregue el product.js, haciendo que aparezca la data dinamica de cada producto cuando toca ver detalle

---
+++ js/product.js
@@ -0,0 +1,48 @@
+let queryString = location.search;
+let queryStringObj = new URLSearchParams(queryString);
+let productId = queryStringObj.get('id');
+
+
+
+let productName = document.querySelector('.productName');
+let productBrand = document.querySelector('.productBrand');
+let productDescription = document.querySelector('.productDescription');
+let productPrice = document.querySelector('.productPrice');
+let productImage = document.querySelector('.productImage');
+let productCategory = document.querySelector('.productCategory');
+let productStock = document.querySelector('.productStock');
+let productTags = document.querySelector('.tags');
+
+
+
+
+
+
+fetch('https://dummyjson.com/products/' + productId)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+
+        console.log(data);
+
+        productName.innerText = data.title;
+        if (data.brand == undefined) {
+        productBrand.innerText = '';
+        } else {
+        productBrand.innerText = data.brand;
+        }
+        productDescription.innerText = data.description;
+        productPrice.innerText = '$' + data.price;
+        productImage.src = data.thumbnail;
+        productImage.alt = data.title;
+        productCategory.innerText = data.category;
+        productCategory.href = './category.html?category=' + data.category;
+        productStock.innerText = data.stock + ' unidades disponibles';
+
+       
+
+    })
+    .catch(function(error) {
+        console.log(error);
+    });
