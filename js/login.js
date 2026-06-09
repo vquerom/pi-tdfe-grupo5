b035909c94601e21b2caea1a72b8f5720637b4bb
vquerom <valenquerom@gmail.com>
2026-06-09 10:43:52 -0300

    login

---
+++ js/login.js
@@ -0,0 +1,20 @@
+let mail = document.querySelector("#email")
+let contraseña = document.querySelector("#password")
+let formulario = document.querySelector("#login")
+
+formulario.addEventListener("submit", function(event){
+
+    if (contraseña.value==""){
+    e.preventDefault();
+    alert("EL campo no puede estar vacio")
+    }else if ((contraseña.value.length < 6)){
+    e.preventDefault();
+    alert("La contraseña debe tener al menos 6 caracteres")
+    }
+    if (mail.vlaue==""){
+        e.preventDefault
+        alert("El campo no puede estar vacio")
+    }else {
+        this.sumbit()
+    }
+})
