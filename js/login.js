f3281d8b50822300779fa4f542815134e9dc4356
vquerom <valenquerom@gmail.com>
2026-06-12 18:17:13 -0300

    corrigiendo el 7a y b

--- js/login.js
+++ js/login.js
@@ -2,7 +2,7 @@ let mail = document.querySelector('.email');
 let password = document.querySelector('.password');
 let form = document.querySelector('.login');
 
-formulario.addEventListener('submit', function(e) {
+form.addEventListener('submit', function(e) {
     e.preventDefault();
 
     if (mail.value == "") {
@@ -22,8 +22,5 @@ formulario.addEventListener('submit', function(e) {
 
         this.submit();
     }
-<<<<<<< HEAD
+
 })
-=======
-});
->>>>>>> 1a3c3d2cb5a8fc48cecb41890eabb1af3c6e525e
