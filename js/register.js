1c4033b1580d0ba5b5b8661adfdcfdf44232c840
vquerom <valenquerom@gmail.com>
2026-06-21 17:56:10 -0300

    falto agregar algo de la consigna

--- js/register.js
+++ js/register.js
@@ -2,6 +2,7 @@ let mail = document.querySelector('.email');
 let password = document.querySelector('.password');
 let password2 = document.querySelector('.password2');
 let form = document.querySelector('.reg');
+let checkbox = document.querySelector('.checkbox');
 
 form.addEventListener('submit', function(e) {
     e.preventDefault();
@@ -14,6 +15,9 @@ form.addEventListener('submit', function(e) {
         alert("La contraseña debe tener al menos 6 caracteres");
     } else if (password.value != password2.value) {
         alert("La contraseñas no coinciden")
+    } else if (checkbox.checked == false) {
+        alert("Debe aceptar los terminos y condiciones");
+    
     } else {
         let datos = {
             email: mail.value
